5f23599838274d6a91321cf78cfb615e517992f4
Ambre Bidault <ambrebidault@orange.fr>
2023-08-17 00:57:32 +0200

    correction du bug formulaire avec apparition du message de succès après l'envoit du formaulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -7,14 +7,13 @@ import Button, { BUTTON_TYPES } from "../../components/Button";
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
+  const [sending, setSending] = useState();
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
       setSending(true);
-      // We try to call mockContactApi
       try {
-        await mockContactApi();
+        await mockContactApi().then(onSuccess);
         setSending(false);
       } catch (err) {
         setSending(false);
