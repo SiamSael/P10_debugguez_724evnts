2e4cf86f56a1af34c3f74fc1b466f49c87b4f0bf
Ambre Bidault <ambrebidault@orange.fr>
2023-08-17 10:49:53 +0200

    correctifs initialisation du state

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -7,7 +7,7 @@ import Button, { BUTTON_TYPES } from "../../components/Button";
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState();
+  const [sending, setSending] = useState(false);
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
